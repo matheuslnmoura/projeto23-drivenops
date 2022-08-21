7cdecb384a7271bcd7c2bf7e4da19dd5b957441d
Matheus Moura <matheuslnmoura@gmail.com>
2022-08-21 08:17:03 -0300

    Feat: New Feature

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -8,6 +8,7 @@ app.use(json());
 
 app.get("/students", async (req: Request, res: Response) => {
   const students = await prisma.student.findMany();
+  console.log('new feature')
   res.send(students);
 });
 
