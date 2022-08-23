020b4e0a2acf3806b36229d91f3cfa8b2fed6628
Matheus Moura <matheuslnmoura@gmail.com>
2022-08-23 11:34:22 -0300

    Remove console.log

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -3,28 +3,20 @@ import axios from "axios";
 
 function App() {
   const [student, setStudent] = useState(null);
-  console.log('chamou o app')
     
   useEffect(async () => {
-    console.log('entrou no use effect')
 
     const API_URL = process.env.REACT_APP_BASE_URL;
     try {
-      console.log('entrou no try')
-
       const response = await axios.get(`${API_URL}/students/random`);
       const student = response.data;
-      console.log("student: ")
-      console.log(student)
       if(!student){
         alert("Putz! Não há estudantes cadastrados para o sorteio!");
       } else {
-        console.log('entrou no else')
 
         setStudent(student);
       }
     } catch (error) {
-      console.log('entrou no catch')
       alert("Não foi possível realizar o sorteio!");
     }
   }, []);
